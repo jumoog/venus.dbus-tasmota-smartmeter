ec8b1fba858df7765d40e10088acd245c9f8401d
Kilian von Pflugk <github@jumoog.io>
2025-11-08 21:49:36 +0000

    Refactor dbus-tasmota-smartmeter.py for clarity

--- dbus-tasmota-smartmeter.py
+++ dbus-tasmota-smartmeter.py
@@ -10,9 +10,9 @@
 ##########################################
 
 
- 
+
 # import normal packages
-import platform 
+import platform
 import logging
 import sys
 import os
@@ -24,9 +24,7 @@ else:
 import sys
 import time
 import requests # for http GET
-from requests.exceptions import RequestException
-from urllib3.exceptions import MaxRetryError
- 
+
 # our own packages from victron
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/opt/victronenergy/dbus-systemcalc-py/ext/velib_python'))
 from vedbus import VeDbusService
@@ -35,42 +33,42 @@ class DbusSmartmeterService:
   def __init__(self, servicename, deviceinstance, paths, productname='Tasmota', connection='Tasmota Web service'):
     self._dbusservice = VeDbusService("{}.http_{:02d}".format(servicename, deviceinstance), register=False)
     self._paths = paths
- 
+
     logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
- 
+
     # Create the management objects, as specified in the ccgx dbus-api document
     self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
     self._dbusservice.add_path('/Mgmt/ProcessVersion', 'Unkown version, and running on Python ' + platform.python_version())
     self._dbusservice.add_path('/Mgmt/Connection', connection)
- 
+
     # Create the mandatory objects
     self._dbusservice.add_path('/DeviceInstance', deviceinstance)
     self._dbusservice.add_path('/ProductId', 45069) # found on https://www.sascha-curth.de/projekte/005_Color_Control_GX.html#experiment - should be an ET340 Engerie Meter
-    self._dbusservice.add_path('/DeviceType', 345) # found on https://www.sascha-curth.de/projekte/005_Color_Control_GX.html#experiment - should be an ET340 Engerie Meter																																										   
+    self._dbusservice.add_path('/DeviceType', 345) # found on https://www.sascha-curth.de/projekte/005_Color_Control_GX.html#experiment - should be an ET340 Engerie Meter
     self._dbusservice.add_path('/ProductName', productname)
-    self._dbusservice.add_path('/CustomName', productname)    
-    self._dbusservice.add_path('/Latency', None)    
+    self._dbusservice.add_path('/CustomName', productname)
+    self._dbusservice.add_path('/Latency', None)
     self._dbusservice.add_path('/FirmwareVersion', 0.1)
     self._dbusservice.add_path('/HardwareVersion', 0)
     self._dbusservice.add_path('/Connected', 1)
     self._dbusservice.add_path('/Role', 'grid')
     self._dbusservice.add_path('/Position', 0) # normaly only needed for pvinverter
     self._dbusservice.add_path('/UpdateIndex', 0)
- 
+
     # add path values to dbus
     for path, settings in self._paths.items():
       self._dbusservice.add_path(
         path, settings['initial'], gettextcallback=settings['textformat'], writeable=True, onchangecallback=self._handlechangedvalue)
- 
+
     # last update
     self._lastUpdate = 0
     self._dbusservice.register()
- 
+
     # add _update function 'timer'
     gobject.timeout_add(1000, self._update) # pause 1s before the next request
-     
-  def _update(self):   
-  
+
+  def _update(self):
+
     try:
       meter_url = "http://192.168.178.95/cm?cmnd=status%2010"
       meter_r = requests.get(url=meter_url) # request data from the Tasmota Smartmeter
@@ -90,8 +88,8 @@ class DbusSmartmeterService:
        #self._dbusservice['/Ac/L3/Power'] = 59
       self._dbusservice['/Ac/L1/Energy/Forward'] = meter_data['StatusSNS']['E320']['Total_in']
        #self._dbusservice['/Ac/L2/Energy/Forward'] = 0.059
-       #self._dbusservice['/Ac/L3/Energy/Forward'] = 0.059																				  
-      self._dbusservice['/Ac/L1/Energy/Reverse'] = meter_data['StatusSNS']['E320']['Total_out'] 
+       #self._dbusservice['/Ac/L3/Energy/Forward'] = 0.059
+      self._dbusservice['/Ac/L1/Energy/Reverse'] = meter_data['StatusSNS']['E320']['Total_out']
       self._dbusservice['/Ac/Energy/Forward'] = self._dbusservice['/Ac/L1/Energy/Forward']
       self._dbusservice['/Ac/Energy/Reverse'] = self._dbusservice['/Ac/L1/Energy/Reverse']
       #logging
@@ -99,30 +97,29 @@ class DbusSmartmeterService:
       logging.debug("House Forward (/Ac/Energy/Forward): %s" % (self._dbusservice['/Ac/Energy/Forward']))
       logging.debug("House Reverse (/Ac/Energy/Revers): %s" % (self._dbusservice['/Ac/Energy/Reverse']))
       logging.debug("---");
-       
-       # increment UpdateIndex - to show that new data is available
-      index = self._dbusservice['/UpdateIndex'] + 1  # increment index
-      if index > 255:   # maximum value of the index
-       index = 0 # overflow from 255 to 0
-      self._dbusservice['/UpdateIndex'] = index
+
+      # increment UpdateIndex - to show that new data is available an wrap
+      self._dbusservice['/UpdateIndex'] = (self._dbusservice['/UpdateIndex'] + 1 ) % 256
 
       #update lastupdate vars
-      self._lastUpdate = time.time()              
-    except MaxRetryError:
-      return True
-    except RequestException as e:
-      logging.debug('HTTP error while retrieving smartmeter data: %s', e)
-      return True
+      self._lastUpdate = time.time()
+    except (ValueError, requests.exceptions.ConnectionError, requests.exceptions.Timeout, ConnectionError) as e:
+      self._dbusservice['/Ac/L1/Power'] = 0
+      self._dbusservice['/Ac/L1/Energy/Forward'] = 0
+      self._dbusservice['/Ac/L1/Energy/Reverse'] = 0
+      self._dbusservice['/Ac/Energy/Forward'] = 0
+      self._dbusservice['/Ac/Energy/Reverse'] = 0
+      self._dbusservice['/UpdateIndex'] = (self._dbusservice['/UpdateIndex'] + 1 ) % 256
     except Exception as e:
       logging.critical('Error at %s', '_update', exc_info=e)
-       
+
      # return true, otherwise add_timeout will be removed from GObject - see docs http://library.isr.ist.utl.pt/docs/pygtk2reference/gobject-functions.html#function-gobject--timeout-add
     return True
- 
+
   def _handlechangedvalue(self, path, value):
     logging.debug("someone else updated %s to %s" % (path, value))
     return True # accept the change
- 
+
 
 
 def main():
@@ -134,20 +131,20 @@ def main():
                                 logging.FileHandler("%s/current.log" % (os.path.dirname(os.path.realpath(__file__)))),
                                 logging.StreamHandler()
                             ])
- 
+
   try:
       logging.info("Start");
-  
+
       from dbus.mainloop.glib import DBusGMainLoop
       # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
       DBusGMainLoop(set_as_default=True)
-     
-      #formatting 
+
+      #formatting
       _kwh = lambda p, v: (str(round(v, 2)) + 'KWh')
       _a = lambda p, v: (str(round(v, 1)) + 'A')
       _w = lambda p, v: (str(round(v, 1)) + 'W')
-      _v = lambda p, v: (str(round(v, 1)) + 'V')   
-     
+      _v = lambda p, v: (str(round(v, 1)) + 'V')
+
       #start our main-service
       pvac_output = DbusSmartmeterService(
         servicename='com.victronenergy.grid',
@@ -156,10 +153,10 @@ def main():
           '/Ac/Energy/Forward': {'initial': 0, 'textformat': _kwh}, # energy bought from the grid
           '/Ac/Energy/Reverse': {'initial': 0, 'textformat': _kwh}, # energy sold to the grid
           '/Ac/Power': {'initial': 0, 'textformat': _w},
-          
+
           '/Ac/Current': {'initial': 0, 'textformat': _a},
           '/Ac/Voltage': {'initial': 0, 'textformat': _v},
-          
+
           '/Ac/L1/Voltage': {'initial': 0, 'textformat': _v},
           '/Ac/L2/Voltage': {'initial': 0, 'textformat': _v},
           '/Ac/L3/Voltage': {'initial': 0, 'textformat': _v},
@@ -176,10 +173,10 @@ def main():
           '/Ac/L2/Energy/Reverse': {'initial': 0, 'textformat': _kwh},
           '/Ac/L3/Energy/Reverse': {'initial': 0, 'textformat': _kwh},
         })
-     
+
       logging.info('Connected to dbus, and switching over to gobject.MainLoop() (= event based)')
       mainloop = gobject.MainLoop()
-      mainloop.run()            
+      mainloop.run()
   except Exception as e:
     logging.critical('Error at %s', 'main', exc_info=e)
 if __name__ == "__main__":
